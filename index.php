3ed312ca06a00be9ae8841edc4ff4d6eaf22fddb
Vinicius <lorddark.dell@gmail.com>
2024-08-09 20:50:23 -0300

    Aula de bootstrap

--- index.php
+++ index.php
@@ -5,11 +5,153 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Site</title>
+    <link href="bootstrap.min.css" rel="stylesheet"/>
+
 </head>
 <body>
-    
+
+<div class="container">
+
+<div class="row">
+
+
+<div class="col-4"></div>
+<div class="col-4">
+
+<div class="alert alert=sucess" role="alert">
+    Cadastrado com Sucesso!
+</div>
+
+</div>
+<div class="col-4"></div>
+
+
+
+</div>
+
+<div class="row">
+    <div class="col-2"></div>
+</div>
+
 
 <h1> Pagina Inicial </h1>
 
+
+<div class="alert alert-success" role="alert">
+  Cadastrado com Sucesso!
+</div>
+
+
+<button type="button" class="btn btn-primary">Primary</button>
+<button type="button" class="btn btn-secondary">Secondary</button>
+<button type="button" class="btn btn-success">Success</button>
+<button type="button" class="btn btn-danger">Danger</button>
+<button type="button" class="btn btn-warning">Warning</button>
+<button type="button" class="btn btn-info">Info</button>
+<button type="button" class="btn btn-light">Light</button>
+<button type="button" class="btn btn-dark">Dark</button>
+
+<button type="button" class="btn btn-link">Link</button>
+
+<a href="www.google.com.br" class="btn btn-secondary"> Acesse o Google </a>
+
+<table class="table table-striped table-bordered table-hover">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">First</th>
+      <th scope="col">Last</th>
+      <th scope="col">Handle</th>
+    </tr>
+  </thead>
+  <tbody>
+    <tr>
+      <th scope="row">1</th>
+      <td>Mark</td>
+      <td>Otto</td>
+      <td>@mdo</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Jacob</td>
+      <td>Thornton</td>
+      <td>@fat</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td colspan="2">Larry the Bird</td>
+      <td>@twitter</td>
+    </tr>
+  </tbody>
+</table>
+<form>
+  <div class="mb-3">
+    <label for="exampleInputEmail1" class="form-label">Email address</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+    <div id="emailHelp" class="form-text">We'll never share your email with anyone else.</div>
+  </div>
+  <div class="mb-3">
+    <label for="exampleInputPassword1" class="form-label">Password</label>
+    <input type="password" class="form-control" id="exampleInputPassword1">
+  </div>
+  <div class="mb-3 form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Check me out</label>
+  </div>
+  <button type="submit" class="btn btn-primary">Submit</button>
+</form>
+<script src="bootstrap.bundle.min.js"><script/>
 </body>
 </html>
+
+
+/* Microsoft Windows [versão 10.0.19045.4651]
+(c) Microsoft Corporation. Todos os direitos reservados.
+
+C:\Users\Aluno>git config --global user.name "Vinicius"
+
+C:\Users\Aluno>git config --global user.email "lorddark.dell@gmail.com"
+
+C:\Users\Aluno>cd ..
+
+C:\Users>cd ..
+
+C:\>cd c:/xampp/htdocs
+
+c:\xampp\htdocs>
+c:\xampp\htdocs>git clone https://github.com/VinokaBRazilian/PWII.git
+Cloning into 'PWII'...
+remote: Enumerating objects: 8, done.
+remote: Counting objects: 100% (8/8), done.
+remote: Compressing objects: 100% (4/4), done.
+remote: Total 8 (delta 1), reused 8 (delta 1), pack-reused 0
+Receiving objects: 100% (8/8), done.
+Resolving deltas: 100% (1/1), done.
+
+c:\xampp\htdocs>code .
+
+c:\xampp\htdocs>getBootstrap.com
+'getBootstrap.com' não é reconhecido como um comando interno
+ou externo, um programa operável ou um arquivo em lotes.
+
+c:\xampp\htdocs>getbootstrap.com
+'getbootstrap.com' não é reconhecido como um comando interno
+ou externo, um programa operável ou um arquivo em lotes.
+
+c:\xampp\htdocs>get bootstrap.com
+'get' não é reconhecido como um comando interno
+ou externo, um programa operável ou um arquivo em lotes.
+
+c:\xampp\htdocs>cd PWII
+
+c:\xampp\htdocs\PWII>code .
+
+c:\xampp\htdocs\PWII>getBootstrap.com
+'getBootstrap.com' não é reconhecido como um comando interno
+ou externo, um programa operável ou um arquivo em lotes.
+
+c:\xampp\htdocs\PWII>getbootstrap.com
+'getbootstrap.com' não é reconhecido como um comando interno
+ou externo, um programa operável ou um arquivo em lotes.
+
+c:\xampp\htdocs\PWII>
