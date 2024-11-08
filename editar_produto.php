ade40603c0a4fbeb779c38c1f01e06dc9e8ef269
Vinicius <lorddark.dell@gmail.com>
2024-11-07 22:42:38 -0300

    Alter Categorias

--- editar_produto.php
+++ editar_produto.php
@@ -8,7 +8,14 @@
         isset($_POST['Codigo_barras']) && !empty($_POST['Codigo_barras'])) {
 
             include 'conexao.php';
-            $sql = "UPDATE Produtos SET Descricao = '$_POST[Descricao]', Valor = $_POST[Valor], Codigo_barras = $_POST[Codigo_barras] WHERE Id = $_POST[Id]";
+
+            $sql = "UPDATE Produtos SET Descricao = '$_POST[Descricao]',
+             Valor = $_POST[Valor], 
+             Codigo_barras = '$_POST[Codigo_barras]', 
+             categoria_id = $_POST[categoria_id]
+             WHERE Id = $_POST[Id]";
+
+
             $resultado = $conexao->query($sql);
             if ($resultado) {
                 //lógica para mensagem de sucesso
@@ -20,7 +27,7 @@
 
     if (isset($_GET['Id']) && !empty($_GET['Id'])) {
         include 'conexao.php';
-        $sql = "SELECT Id, Descricao, Valor, Codigo_barras FROM produtos WHERE Id = $_GET[Id]";
+        $sql = "SELECT Id, Descricao, Valor, Codigo_barras, categoria_id FROM produtos WHERE Id = $_GET[Id]";
         $resultado = $conexao->query($sql);
         if ($resultado) {
             if ($resultado->num_rows > 0) {
@@ -29,6 +36,7 @@
                     $descricao = $row["Descricao"];
                     $valor = $row["Valor"];
                     $codigo_barras = $row["Codigo_barras"];
+                    $categoria_id = $row["categoria_id"];
                 }
             }
             else {
@@ -49,7 +57,40 @@
     <input name="Descricao" value="<?php echo $descricao ?>"/>
     <input name="Valor" value="<?php echo $valor ?>"/>
     <input name="Codigo_barras" value="<?php echo $codigo_barras ?>"/>
+
     <button type="submit">Salvar alterações</button>
+
+    
+
+    <select name="categoria_id">
+
+    <?php 
+    $sql_categorias = "Select Id, Nome from categorias";
+    $resultado_agora = $conexao->query($sql_categorias);
+
+    if ($resultado_agora->num_rows > 0) {
+        while($row = $resultado_agora->fetch_assoc()) 
+        {
+
+            if($categoria_id == $row['Id'])
+            {
+echo "<option selected value='$row[Id]'> $row[Nome] </option>";
+            }
+            else{
+                echo "<option value='$row[Id]'>$row[Nome] </option>";
+            }
+        }
+    }
+    else
+        {
+    echo "<option value='0'> Não tem categoria cadastrada </option>";
+   
+        }
+   ?>
+       
+</select>    
+
+<br>
 </form>
 
 <?php include "rodape.php"; ?>
